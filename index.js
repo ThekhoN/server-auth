fb5930079f11893e30a0ac72251c42baebb4130d
Ngaosathe <thekho.ngaosathe@jasperindia.local>
2017-04-20 12:41:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// entry
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./router/router');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// db
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:auth/auth');
+
+// app
+const app = express();
+app.use(bodyParser.json({type: '*/*'}));
+app.use(morgan('combined'));
+router(app);
+
+// server
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port, function (err) {
+  if(err){
+    console.log('error in server:', err);
+  }
+  else {
+    console.log('app listening on port: ', port);
+  }
+});
